119f81db65340949638eba39026dae875cbb8002
Chris Wilson <christopher.e.wilson@gmail.com>
2013-02-10 19:33:40 -0500

    chaplains.py looks up bioguide ID

--- README.md
+++ README.md
@@ -18,13 +18,16 @@ Options:
 
 `task`: Which task to perform. Options are:
 - `collect`: Scrape the prayers from the House Chaplain website 
-- `combine`: Combine into one file
-- `index`: Create a concordance index of every word in every prayer
+- `combine`: Combine all prayers in /data/ directory into one file
+- `index`: Create a concordance index of every word in every prayer in the /data/directory
 
+####collect params
 `--limit`: maximum number of prayers to retrieve, combine, or index. Default is all of them
 
-Example:
+####combine params
+`--ignore`: fields in the individual prayer files to leave out of combined file to reduce size. Options include any top-level key in sample data structure below
 
+Example:
 ```
 python ./scripts/chaplains.py collect --limit=100
 ```
@@ -34,14 +37,43 @@ As of February 8, 2013, there are 633 such guest prayers on the website. This sc
 
 ```
 {
-  "name": "Reverend Paul A. Stoot, Sr", 
-  "opening": "O Lord our God, if ever we needed Thy wisdom and Thy guidance, it is now as this honorable body of great men and women begin a new day, a day that will hold many opportunities and many possibilities.\nWe pray that You will bless these men and these women who have been chosen by the great people of this great Nation, for You know them and You know their needs, You know their motives and their hopes and their fears. Lord Jesus, put Your arms around them and give them strength and speak to them to give them wisdom greater than their own. May they hear Your voice as You speak to them and as they seek to hear from You and Your guidance.\nMay they remember that You are concerned about what is said and what is done here and may they ever have a clear conscience before Thee, that they need fear no man. Bless us each according to our deepest needs as we are here today to use us to Your honor and to Your glory, we humbly ask. Amen.", 
-  "state": "WA", 
-  "sponsor": "Rep. Rep. Rick Larsen, (D-WA)", 
-  "church": "Greater Trinity Missionary Baptist Church", 
-  "date": "06/21/2001", 
-  "party": "D", 
-  "id": "1142", 
-  "location": "Everett, WA"
+  "name": "Reverend Daniel P. Coughlin", 
+  "opening": "Almighty God,\nwe pause this morning to give You thanks.\nWe thank you for another day to live and work in the USA.\nWe thank you for your Word, which teaches us righteousness and justice and equity.\nWe thank you for giving us a system of government that honors our dignity and checks our depravity.\nWe repent for the spirit of self\u2013sufficiency that tells us we do not need You, and the spirit of arrogance that tells us we do not need others.\nOn this first day of May, dear Father, we make a new commitment to live out our nation's motto, \"In God we trust.\"\nFor You are the Rock of our salvation,\nYou are our Hiding Place in times of terror,\nYou are the Truth in Whom we can rely.\nOur nation needs a third great spiritual awakening.  May it begin here, with each of us.\nBless these men and women, who represent America.\nBless them in their deliberations to seek and find Your wisdom, that Thy will be done on earth as it is in heaven.\nHear our prayer, for you are our Lord and Savior.  Amen.", 
+  "introduction": "Mr. Speaker, I rise this morning to welcome my good friend, Pastor Jim Congdon, to the floor of the House of Representatives.  It was my distinct honor to invite Pastor Congdon to deliver our opening prayer this morning, and I am grateful for his willingness to do so.\nPastor Congdon is from Topeka, Kansas, and pastors the Topeka Bible Church, which my family and I attended for several years.  It was a privilege to call Jim my pastor, and I am grateful for his continued friendship and dedication to the ministry.\nI also want to welcome Jim's wife, Melody, to the House Chamber.  I know she came with Jim and she speaks for a lot of different people in the church.  She is a constant source of strength and support in the ministry at the Topeka Bible Church.\nFinally, I want to welcome the 62 students from Cair Paravel Latin School that are seated in the gallery.  Pastor Congdon teaches philosophy at Cair Paravel, and these 9th and 10th graders are in D.C. for part of their studies.  I thank them all for being here.\nI thank Jim and Melody for their presence, and God bless them.;", 
+  "member": {
+    "party": "R", 
+    "state": "KS", 
+    "name": "Jim R. Ryun", 
+    "bioguide": "R000566"
+  }, 
+  "session": 107, 
+  "church": "Topeka Bible Church", 
+  "date": "05/01/2002", 
+  "uid": "1069", 
+  "location": "Topeka, KS"
 }
 ```
+
+## Internal method of member lookup
+To get the [Congressional Bioguide](http://bioguide.congress.gov/biosearch/biosearch.asp) ID for each member, I wrote a small library called `member_lookup.py`, which compares the plaintext name of the member to the NYT API for that session
+
+Here's how it works:
+- If fed a string for the name, the function assumes it's in first, middle, last order. Can also accept a list of names in that order
+- If there are three names, not including initials, it adds both the surname and a hyphenated middle-last name to the list of names to search, since no one can agree on whether Sheila Jackson Lee uses a hyphen
+- Likewise, if fed a surname with a hyphen, this function also searches for that name with just a space and with just the second of the two names
+- The function then finds all members of the inputed session and chamber with a matching surname. Often this is just one person, so we have a high-confidence match
+- If multiple members for the inputed session have the same surname, it then reduces the list of possible matches by state and then first name.
+- If there's still no good match, when then start comparing the inputed surname to all members, allowing for an edit distance of one, which will catch common misspellings without too high a risk of false positives
+
+Members this function can find in spite of errors on House Chaplain website:
+- "Rep. Rep. Jim Ryn (R-KS)" correctly finds Jim Ryan
+- "Rep. Rep. Deborah Halverson (D-IL)" finds Debbie Halvorson
+- "Rep. Jean Schmidt (R-AL)" finds Jean Schmidt of Ohio
+
+Members who currently trip up the function for TK reason:
+- Rep. Hon. Stephanie Herseth Sandlin, (D-SD) (110)
+- Rep. Hon. John Spratt, (D-SC) (109)
+- Rep. Cathy McMorris, (R-WA) (109)
+- Rep. Hon. Tom Davis, (R-VA) (108)
+- Rep. Chris Van Hollen, (D-MD) (108)
+- Rep. John Duncan, Jr., (R-TN) (107)
--- scripts/chaplains.py
+++ scripts/chaplains.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
+
 import os
 import sys
 import json
 import argparse
 import re
+import math
+from member_lookup import lookup
 from collections import defaultdict
 from utils import download, write, uniq
 from lxml import etree
@@ -10,12 +14,13 @@ from lxml.html import fromstring
 
 #crawl the House Chaplain Web site and download all guest prayers. Caches by default
 def collect(options = {}):
+    #landing page with links to all guest prayers
     page  = fromstring(download('http://chaplain.house.gov/chaplaincy/guest_chaplains.html'))
-    links = page.xpath("//td/a/@href")
+    links = uniq(page.xpath("//td/a/@href"))
     limit = options.get("limit", False)
     if limit:
         links = links[:limit]
-        
+    
     for link in links:
         try:
             uid = link.split('id=')[1]
@@ -25,14 +30,16 @@ def collect(options = {}):
         html = fromstring(download('http://chaplain.house.gov/chaplaincy/' + link, uid + '.html'))
         info = {}
         info['name'] = html.xpath("//h2/text()")[0]     
+        
+        #get h3 pairings, guess whether a church is listed based on number of hits
         hs = html.xpath("//h3/text()")
         if len(hs) > 1:
             info['church'] = hs[0].strip()
             info['location'] = hs[1].strip()
         else:
-            #print hs
             info['location'] = hs[0].strip()
-            
+
+        # get boldface pairings
         for pair in html.xpath('//strong'):
             if pair.tail:
                 label, data = pair.text.replace(':', '').strip(), pair.tail.strip()
@@ -41,37 +48,57 @@ def collect(options = {}):
                 label, data = pair.text.replace(':', '').strip(), pair.getnext().xpath("text()")[0].strip()
                 info[label.lower().split(" ")[0]] = data
     
+        # add h4/p pairings
         for pair in html.xpath('//h4'):
             if pair.getnext().tag == 'p':
                 label, data = pair.text.replace(':', '').strip(), '\n'.join([x.strip() for x in pair.getnext().xpath("text()")])
                 info[label.lower().split(" ")[0]] = data
         if "one" in info:
+            info["introduction"] = info["one"]
             info.pop("one")
-        info['id'] = uid
+
+        #sessions
+        info["session"] = int(math.floor((int(info["date"].split("/")[-1]) - 1789) / 2) + 1)
+        info['uid'] = uid
+        info['member'] = {}
+        
+        #get bioguide match for sponsor
         if 'sponsor' in info:
             #fix a recurring typo on House Chaplain website            
+            info['member'] = {}
             info['sponsor'] = info['sponsor'].replace("Rep. Rep.", "Rep.")
+            pieces = re.search("\s(.+?), \(([A-Z])-([A-Z]{2})\)", info['sponsor']).groups()
+            info['member']['name'] = pieces[0]
+            info['member']['party'] = pieces[1]
+            info['member']['state'] = pieces[2]
+            member_info = lookup(info['member']['name'], info['session'], info['member']['state'], 'house')
             
-            m = re.findall("\(([A-Z])-([A-Z]+)\)", info["sponsor"])
-            info['party'] = m[0][0]
-            info['state'] = m[0][1]
-        else:
-            info['party'] = 'N'
-            info['state'] = ''
-            
+            if member_info['status'] == 'Found':
+                #use name info from API instead since it's more canonical 
+                if not member_info['middle_name']:
+                    member_info['middle_name'] = ''
+                info['member']['name'] = member_info["first_name"] + " " + member_info['middle_name'] + " " + member_info['last_name']
+                info['member']['name'] = info['member']['name'].replace("  ", " ")
+                info['member']['state'] = member_info["state"]
+                info['member']['bioguide'] = member_info['id']
+            else:
+                print member_info['status'], info['member']['name']
+                print "Unable to find %s (%d) in the NYT API" % (info['sponsor'], info['session'])                  
+                info['member']['bioguide'] = None
+            info.pop("sponsor")
         write(json.dumps(info, indent=2), os.getcwd() + "/data/" +  uid + ".json")
-        #print uid
 
-#create a single file for use in browser. 723kb without indentation
-def combine(options = {}):
+#create a single file for use in browser, from whatever we find in the /data/ folder. 723kb without indentation
+def combine(options):
     alld  = {}
     files = [x for x in os.listdir(os.getcwd() + "/data/") if re.sub("\d+\.json", "", x) == ""]
-    if options.get('limit', False):
-        files = files[:options.get('limit')]
-        
+    ignore = options.get("ignore", [])
     for file in files:
         sermon = json.load(open(os.getcwd() + "/data/" + file, 'r'))        
-        alld[sermon['id']] = sermon
+        for i in ignore:
+            if i in sermon:
+                sermon.pop(i)
+        alld[sermon['uid']] = sermon
     write(json.dumps(alld, indent=2, sort_keys=True), os.getcwd() + "/data/all.json")
     write(json.dumps(alld, sort_keys=True), os.getcwd() + "/data/all.min.json")
 
@@ -85,7 +112,7 @@ def index(options = {}):
     for file in files:
         sermon = json.load(open(os.getcwd() + "/data/" + file, 'r'))
         words = uniq(re.findall(r"\b[A-z]+\b", sermon['opening'].replace('\n', ' ').lower()))
-        
+                
         '''
         if options.get("uniques", False):
             words = uniq(re.findall(r"\b[A-z]+\b", sermon['opening'].replace('\n', ' ').lower()))
@@ -105,16 +132,18 @@ def main():
                         help="which task to run: collect, combine, index")
     parser.add_argument("-l", "--limit", metavar="INTEGER", dest="limit", type=int, default=None,
                         help="minimum number of invocations to retrieve")
+    parser.add_argument("-i", "--ignore", metavar="string", dest="ignore", type=str, default='',
+                        help="fields not to include in combo file")
     args = parser.parse_args()
-    
+        
     if args.task == "collect":
         collect({ 'limit': args.limit })
     elif args.task == "combine":
-        combine({ 'limit': args.limit })    
+        combine({'ignore': args.ignore.split(",")})    
     elif args.task == "index":
         index({ 'limit': args.limit })    
-    elif args.task == "find":
-        find()    
+    else:
+        print "Unknown task (options are collect, combine, index)"
     
 if __name__ == "__main__":
     main()
